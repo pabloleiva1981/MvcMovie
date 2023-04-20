23e73e198c95c7cfdd63878a039fa5deea1ec069
Pablo Ariel Leiva <pablo_ariel_leiva@hotmail.com>
2023-04-20 13:08:33 -0300

    Tutorial 4

---
+++ MvcMovie/Controllers/MoviesController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcMovie.Models;
+
+namespace MvcMovie.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public async Task<IActionResult> Index()
+        {
+            var listMovies = new List<Movie>();
+
+            var movie1 = new Movie
+            {
+                Genre = "Terror",
+                Id = 1,
+                Price = 1,
+                ReleaseDate = DateTime.Now,
+                Title = "La noche del terror"
+            };
+            listMovies.Add(movie1);
+
+            var movie2 = new Movie
+            {
+                Genre = "Terror",
+                Id = 1,
+                Price = 1,
+                ReleaseDate = DateTime.Now,
+                Title = "La noche del terror II"
+            };
+            listMovies.Add(movie2);
+
+            return View(listMovies);
+
+        }
+
+        // GET: Movies/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound(); // TODO: Agregar una vista
+            }
+
+            //Simulación de creación de un objeto (model)
+            //Mas adelante vamos a ver como usar una base de datos
+            var movie = new Movie
+            {
+                Genre = "Terror",
+                Id = 1,
+                Price = 1,
+                ReleaseDate = DateTime.Now,
+                Title = "La noche del terror"
+            };
+
+
+            return View(movie);
+        }
+    }
+}
