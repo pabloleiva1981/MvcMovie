383ab039d5055aa3b4b60826d30faa6cc0010cba
Pablo Ariel Leiva <pablo_ariel_leiva@hotmail.com>
2023-06-19 19:35:05 -0300

    Semana 11

--- MvcMovie/Controllers/MoviesController.cs
+++ MvcMovie/Controllers/MoviesController.cs
@@ -1,38 +1,47 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MvcMovie.Models;
+using Microsoft.Data.SqlClient;
 
 namespace MvcMovie.Controllers
 {
     public class MoviesController : Controller
     {
+        private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=DBMovies;Integrated Security=True;Connect Timeout=30;Encrypt=False;";
+        public static List<Movie> movies = new List<Movie>();
         // GET: Movies
         public IActionResult Index()
         {
-            var listMovies = new List<Movie>();
-
-            var movie1 = new Movie
+            try
             {
-                Genre = "Terror",
-                Id = 1,
-                Price = 1,
-                ReleaseDate = DateTime.Now,
-                Title = "La noche del terror"
-            };
-            listMovies.Add(movie1);
+                movies.Clear();
+                SqlConnection connection = new SqlConnection(connectionString);
+                connection.Open();
 
-            var movie2 = new Movie
+                string queryString = "SELECT * FROM Movies";
+                SqlCommand command = new SqlCommand(queryString, connection);
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Movie movie = new Movie()
+                    {
+                        Id = int.Parse(reader[0].ToString()),
+                        Genre = reader[1].ToString(),
+                        Price = (decimal)Double.Parse(reader[2].ToString()),
+                        ReleaseDate = DateTime.Parse(reader[3].ToString()),
+                        Title = reader[4].ToString()
+                    };
+                    movies.Add(movie);
+                }
+                connection.Close();
+                return View(movies);
+            }
+            catch (Exception ex)
             {
-                Genre = "Terror",
-                Id = 1,
-                Price = 1,
-                ReleaseDate = DateTime.Now,
-                Title = "La noche del terror II"
-            };
-            listMovies.Add(movie2);
+                throw;
+            }
 
-            return View(listMovies);
-
-        }
+           }
 
         // GET: Movies/Details/5
         public async Task<IActionResult> Details(int? id)
@@ -67,7 +76,53 @@ namespace MvcMovie.Controllers
 
         public async Task<IActionResult> Delete(int? id)
         {
-            return View();
+            try
+            {
+                SqlConnection connection = new SqlConnection(connectionString);
+                connection.Open();
+                string queryString = "SELECT * FROM Movies where id=@Id;";
+                SqlCommand command = new SqlCommand(queryString, connection);
+                command.Parameters.AddWithValue("@Id", id);
+                SqlDataReader reader = command.ExecuteReader();
+                Movie movie = null;
+                while (reader.Read())
+                {
+                    movie = new Movie()
+                    {
+                        Id = (int)reader[0],
+                        Genre = reader[1].ToString(),
+                        Price = (decimal)Double.Parse(reader[2].ToString()),
+                        ReleaseDate = (DateTime)reader[3],
+                        Title = reader[4].ToString()
+                    };
+                }
+                connection.Close();
+                return View(movie);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public ActionResult HandleDelete(int? id)
+        {
+            try
+            {
+                SqlConnection connection = new SqlConnection(connectionString);
+                connection.Open();
+                string queryString = "DELETE FROM Movies where id=@Id;";
+                SqlCommand command = new SqlCommand(queryString, connection);
+                command.Parameters.AddWithValue("@Id", id);
+
+                SqlDataReader reader = command.ExecuteReader();
+                connection.Close();
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
         }
     }
 }
---
+++ SQLQuery1.sql
@@ -0,0 +1,3 @@
+﻿INSERT INTO [dbo].[Movies]( [Genre], [Price], [ReleaseDate], [Title]) VALUES (N'Terror', 250, N'2023-06-19', N'Nueva pelicula')
+INSERT INTO [dbo].[Movies]( [Genre], [Price], [ReleaseDate], [Title]) VALUES (N'Comedia', 650, N'2023-06-19', N'Otra pelicula')
+INSERT INTO [dbo].[Movies]( [Genre], [Price], [ReleaseDate], [Title]) VALUES (N'Drama', 1450, N'1996-06-19', N'La lista de Schindler')
